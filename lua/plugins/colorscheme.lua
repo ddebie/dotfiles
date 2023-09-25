355e8cf7c4595951593bb6d66f0a9ec62630e706
Dillon de Bie <dillondebie@gmail.com>
2023-09-25 20:56:22 +1000

    secondary catppuccin scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -27,5 +27,31 @@ return {
     init = function()
       vim.cmd([[colorscheme dracula]])
     end,
+  },
+  { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000,
+    opts = function(_, opts)
+        local colors = require("catppuccin.palettes").get_palette("mocha")
+        local active_bg = "#1c1c1c"  -- color234
+        local inactive_bg = "#303030"  -- color236
+        local selection_bg = "#262626"
+
+        return {
+            custom_highlights = {
+                Normal = { fg = colors.text, bg = "None" },
+                NormalNC = { fg = colors.text, bg = inactive_bg },
+                CursorLine = { bg = selection_bg },
+
+                TelescopeNormal = { fg = colors.text, bg = active_bg },
+                TelescopeBorder = { fg = colors.text, bg = active_bg },
+                TelescopePromptBorder = { fg = colors.text, bg = active_bg },
+                TelescopeResultsBorder = { fg = colors.text, bg = active_bg },
+                TelescopePreviewBorder = { fg = colors.text, bg = active_bg },
+                TelescopeSelection = { fg = colors.text, bg = selection_bg },
+            }
+        }
+    end,
   }
 }
