e8b83031fe8e203e6e51c53a590947a09adf682c
Dillon de Bie <dillondebie@gmail.com>
2023-09-25 20:42:27 +1000

    autoread

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -9,3 +9,11 @@ vim.api.nvim_create_autocmd(
         desc = 'Automatically resize windows when the host window size changes.'
     }
 )
+
+-- Auto reload file when changed on disk (like git checkout for example)
+-- https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482/3
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
