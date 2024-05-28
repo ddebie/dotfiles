1f0d199469f3b31d0224c6dead031f5f03274f24
Dillon de Bie <dillon.debie@tibra.com>
2024-05-28 11:18:48 +1000

    update nvim plugins and suppress change_detection

--- nvim-lazy/nvim/init.lua
+++ nvim-lazy/nvim/init.lua
@@ -16,4 +16,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    -- Suppress the annoying notification that comes up whenever you change dotfiles
+	change_detection = { notify = false },
+})
