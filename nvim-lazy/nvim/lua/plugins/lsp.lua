35e9df617b27cc84e443a800ef59bfb26c6bdb5a
Dillon de Bie <dillondebie@gmail.com>
2024-11-09 18:07:20 +1100

    update plugins, nvim-surround

--- nvim-lazy/nvim/lua/plugins/lsp.lua
+++ nvim-lazy/nvim/lua/plugins/lsp.lua
@@ -48,29 +48,29 @@ return {
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
                 local map = function(m, lhs, rhs, desc)
-                  vim.keymap.set(m, lhs, rhs, {buffer = bufnr, desc = desc})
+                    vim.keymap.set(m, lhs, rhs, { buffer = bufnr, desc = desc })
                 end
-                map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', 'Hover documentation')
-                map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', 'Go to definition')
-                map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', 'Go to declaration')
-                map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', 'Go to implementation')
-                map('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', 'Go to type definition')
-                map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', 'Go to reference')
-                map('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', 'Show function signature')
-                map('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename symbol')
-                map('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', 'Format file')
-                map('x', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', 'Format selection')
-                map('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Execute code action')
+                map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover documentation")
+                map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", "Go to definition")
+                map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", "Go to declaration")
+                map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", "Go to implementation")
+                map("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Go to type definition")
+                map("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", "Go to reference")
+                map("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Show function signature")
+                map("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename symbol")
+                map("n", "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", "Format file")
+                map("x", "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", "Format selection")
+                map("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Execute code action")
 
                 if vim.lsp.buf.range_code_action then
-                map('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>', 'Execute code action')
+                    map("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>", "Execute code action")
                 else
-                map('x', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', 'Execute code action')
+                    map("x", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", "Execute code action")
                 end
 
-                map('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', 'Show diagnostic')
-                map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', 'Previous diagnostic')
-                map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', 'Next diagnostic')
+                map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", "Show diagnostic")
+                map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Previous diagnostic")
+                map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next diagnostic")
             end)
 
             require("mason-lspconfig").setup({
